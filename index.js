67abb033e566dee00cb9f0066afdbb8f4fe3bfa5
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2022-01-04 13:51:34 -0700

    backend instruments display on front end

--- index.js
+++ index.js
@@ -13,9 +13,27 @@ const getInstruments = () => {
     .then(resp => resp.json())
     .then(instruments => {
         instruments.forEach(i => {
-            let text = document.createElement("p")
-            text.textContent = i.name
-            instrumentContainer.appendChild(text)
+            let div = document.createElement("div")
+            div.className = "instrument"
+            instrumentContainer.appendChild(div)
+
+            let name = document.createElement("h2")
+            let img = document.createElement("img")
+            let instrument = document.createElement("p") 
+            let manufacturer = document.createElement("p")
+            let notes = document.createElement("p")
+
+            notes.textContent = `Why Sara loves it: "${i.notes}"`
+            manufacturer.textContent = `Made by: ${i.manufacturer}`
+            instrument.textContent = i.instrument
+            img.src = i.img_file
+            name.textContent = i.name
+
+            div.appendChild(name)
+            div.appendChild(img)
+            div.appendChild(instrument)
+            div.appendChild(manufacturer)
+            div.appendChild(notes)
         });
     })
 }
