a2de7bdb409b7db75a55660bbb2e43eeb0aa7bab
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2022-01-06 16:53:01 -0700

    refactored getInstruments with helper functions

--- index.js
+++ index.js
@@ -9,50 +9,77 @@ let mainBody = document.querySelector("#main-body")
 let instrumentContainer = document.getElementById("instrument-container")
 console.log(instrumentContainer)
 
+let instrumentsArr = []
+
 const getInstruments = () => {
     fetch("http://localhost:3000/instruments")
     .then(resp => resp.json())
     .then(instruments => {
-        instruments.forEach(i => {
-            let div = document.createElement("div")
-            div.className = "instrument"
-            instrumentContainer.appendChild(div)
-
-            let name = document.createElement("h2")
-                name.textContent = i.name
-                name.className = "hide"
-            
-            let img = document.createElement("img")
-                img.src = i.img_file
-            
-            let instrument = document.createElement("h3") 
-                instrument.textContent = i.instrument
-            
-            let manufacturer = document.createElement("p")
-                manufacturer.textContent = `Made by ${i.manufacturer}`
-            
-            let notes = document.createElement("p")
-                notes.textContent = `What Sara says: ${i.notes}`
-            
-
-            div.appendChild(img)
-            div.appendChild(name)
-
-            div.addEventListener("click", () => {
-                instrumentContainer.remove()
-                let details = document.createElement("div")
-                details.className = "intrument-details"
-                mainBody.appendChild(details)
-                details.appendChild(img)
-                details.appendChild(name)
-                details.appendChild(instrument)
-                details.appendChild(notes)
-                details.appendChild(manufacturer)
-                
-            })
-        });
+        setInstruments(instruments)
+        displayInstruments()
     })
 }
 
-getInstruments()
+const setInstruments = (instruments) => {
+    instrumentsArr = instruments
+    console.log(instruments)
+}
 
+const displayInstruments = () => {
+    instrumentsArr.forEach(i => {
+        let div = document.createElement("div")
+        div.className = "instrument"
+        instrumentContainer.appendChild(div)
+
+        let name = document.createElement("h2")
+            name.textContent = i.name
+            name.className = "hide"
+        
+        let img = document.createElement("img")
+            img.src = i.img_file
+        
+        let instrument = document.createElement("h3") 
+            instrument.textContent = i.instrument
+        
+        let manufacturer = document.createElement("p")
+            manufacturer.textContent = `Made by ${i.manufacturer}`
+        
+        let notes = document.createElement("p")
+            notes.textContent = `What Sara says: ${i.notes}`
+        
+
+        div.appendChild(img)
+        div.appendChild(name)
+
+        div.addEventListener("click", () => {
+            instrumentContainer.remove()
+            let details = document.createElement("div")
+            details.className = "instrument-details"
+            let col1 = document.createElement("div")
+            let col2 = document.createElement("div")
+            
+            mainBody.appendChild(details)
+            details.appendChild(col1)
+            details.appendChild(col2)
+            
+            col1.appendChild(img)
+            col2.appendChild(name)
+            col2.appendChild(instrument)
+            col2.appendChild(notes)
+            col2.appendChild(manufacturer)
+            
+            let button = document.createElement("button")
+            button.textContent = "Back"
+            col2.appendChild(button)
+
+            button.addEventListener("click", () => {
+                getInstruments()
+            })
+            
+        })
+    });
+}
+
+console.log(instrumentsArr)
+
+getInstruments()
